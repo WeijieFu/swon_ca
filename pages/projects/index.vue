b90ca28808a5a7894f68ea55eec5d3276aabee85
WeijieFu <weijiefu92@gmail.com>
2020-12-23 15:58:24 +0100

    add axios

---
+++ pages/projects/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="projects">
+        <div>Project List</div>
+        <div class="">{{ip}}</div>
+    </div>
+</template>
+<script>
+export default {
+    async asyncData({ $axios }) {
+        const ip = await $axios.$get('https://swon-strapi.herokuapp.com/articles/3')
+        return { ip }
+    }
+}
+</script>
+<style scoped>
+
+</style>
