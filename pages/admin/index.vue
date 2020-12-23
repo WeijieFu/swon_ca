752d849f0446cb435e15a2edf22ce111ee7ed1a7
WeijieFu <weijiefu92@gmail.com>
2020-12-23 17:39:15 +0100

    add admin page

---
+++ pages/admin/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="admin">
+        <a href="https://swon-strapi.herokuapp.com/admin" target="_blank"><div class="button">Sign in as administrator</div></a>
+    </div>
+</template>
+<style scoped>
+.admin{
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.button{
+    background-color: #000;
+    cursor: pointer;
+    width: 20rem;
+    padding: 1rem;
+    /* border: 1px #000 solid; */
+    text-align: center;
+    border-radius: 1rem;
+
+}
+a{
+    font-size: 1rem;
+    font-family: 'Open Sans' , sans-serif;
+    color: #fff;
+    text-decoration: none;
+    
+}
+</style>
